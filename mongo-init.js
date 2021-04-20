29e46ad83dab6674512e2900f37e416476665382
JuanitoElBailador <elbailador@jamgeo.com>
2021-04-20 12:31:02 +0200

    Add prometheus service Add user for mongodb exporter

--- mongo-init.js
+++ mongo-init.js
@@ -1,17 +1,21 @@
 conn = new Mongo();
-db = conn.getDB("docker-compose")
-db.createUser(
-{
+db = conn.getDB("docker-compose");
+
+db.createUser({
     user: "root",
     pwd: "secure",
     roles: [
-        {
-            role: "readWrite",
-            db: "docker-compose"
-        }
+        { role: "readWrite", db: "docker-compose" }
     ]
-}
-);
+});
+db.getSiblingDB("admin").createUser({
+    user: "mongodb_exporter",
+    pwd: "password",
+    roles: [
+        { role: "clusterMonitor", db: "admin" },
+        { role: "read", db: "docker-compose" }
+    ]
+});
 
 db.user.insert([
 { userId: 'jamg', name: 'Juan' } ,
